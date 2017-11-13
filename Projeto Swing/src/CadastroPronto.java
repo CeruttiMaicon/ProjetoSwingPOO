35925a6070abb9e27c9a86dd7ef299e967f8d40f
CilesioMeurer <cilesio.junior@univille.br>
2017-11-13 21:49:19 -0200

    Adicionamos elementos na tela cadastro pronto

--- Projeto Swing/src/CadastroPronto.java
+++ Projeto Swing/src/CadastroPronto.java
@@ -1,40 +1,61 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 public class CadastroPronto extends JFrame {
-	
+
 	private int min = 0, max = 2, ini = 2;
 
-	
-	public CadastroPronto(){
-		
+	public CadastroPronto() {
 
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Exibe janela
+		setVisible(true);
 
-			// Exibe janela
-			setVisible(true);
+		// nome janela
+		setTitle("Cadastro Foto");
 
-			// nome janela
-			setTitle("Cadastro Foto");
+		// tamanho janela
+		setSize(360, 678);
 
-			// tamanho janela
-			setSize(360, 678);
+		// trava o tamanho da jenela
+		setResizable(true);
 
-			// trava o tamanho da jenela
-			setResizable(true);
-
-			setup();
+		setup();
 	}
-	
-	private void setup(){
+
+	private void setup() {
 		JPanel painel = new JPanel();
 		painel.setLayout(null);
 		painel.setSize(300, 470);
 
-		
-		
+		////////////////////////////////////////////
+
+		// Botão
+
+		JButton butao = new JButton("Próximo");
+		butao.setBounds(40, 555, 265, 30); // (Lado esquerdo, Cima para baixo,
+											// Tamanho largura, Tamanho altura)
+
+		painel.add(butao);
+		add(painel);
+
+		/////////////////////////////////////////////
+
+		/////////////////////////////////////////////
+
+		// JSlider - para mostrar o progresso
+
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, min, max, ini);
+
+		slider.setBounds(30, 578, 280, 40);
+
+		painel.add(slider);
+
+		////////////////////////////////////////////////
+
 	}
 
 }
